67bb301efc108b5734cf524b69760ddbae4a3e3a
qiangsir45 <1983477897@qq.com>
2020-06-01 20:02:47 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/chaofang/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.chaofang.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("git test 01");
+        System.out.println("git test 02");
+        System.out.println("git test 03");
+        System.out.println("git test 04");
+        System.out.println("git test 05");
+
+    }
+}
